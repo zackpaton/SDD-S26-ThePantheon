387695a447d8e093b4c4996488ad759f4e01b10d
code4ry <140927679+code4ry@users.noreply.github.com>
2026-03-31 21:11:33 -0400

    Updated login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,24 +3,21 @@ export default function LoginPage() {
     <div className="flex items-center justify-center h-screen">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-            Username
-          </label>
           <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
-            Password
-          </label>
-          <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
-          <p className="text-red-500 text-xs italic">Please choose a password.</p>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"></input>
         </div>
-        <div className="flex items-center justify-center">
-          <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <div>
+          <button className="w-full bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline" type="button">
             Login
           </button>
-          <a className="inline-block align-baseline font-bold text-right text-purple-500 hover:text-purple-800" href="#">
+          <a className="block align-baseline text-right text-purple-500 hover:text-purple-800" href="#">
             Forgot Password?
+          </a>
+          <br></br>
+          <a className="block align-baseline font-bold text-center text-purple-500 hover:text-purple-800" href="#">
+            Don't have an account? <br></br>Sign up
           </a>
         </div>
       </form>
