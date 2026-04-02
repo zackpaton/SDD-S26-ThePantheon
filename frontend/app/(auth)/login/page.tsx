78babcfcc635d3941d62a8eda878957ac4626098
zackpaton <91792781+zackpaton@users.noreply.github.com>
2026-04-02 03:59:09 -0400

    Prepare main for merge

--- frontend/app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import  LoginForm  from "@/components/forms/LoginForm";
-
-export default function LoginRoute() {
-  return (
-     <LoginForm />
-  )
-}
--- frontend/app/(auth)/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import SignUpForm from "@/components/forms/SignUpForm";
-
-export default function SignUpFunction(){
-  return(
-    <SignUpForm />
-  )
-}
